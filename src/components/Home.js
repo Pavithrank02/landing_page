58a6922a82f6d342496b72e22f37dde3c159f473
Pavithran2 <pavithrank02@gmail.com>
2023-03-18 10:03:13 +0530

    developed home page

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='user-info-container'>
+       Home
+    </div>
+  )
+}
+
+export default Home
