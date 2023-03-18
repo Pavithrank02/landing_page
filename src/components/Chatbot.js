8fa5946111ef4e32250d14b51501491db90b84bf
Pavithran2 <pavithrank02@gmail.com>
2023-03-18 14:06:06 +0530

    chatbot users list displayed

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -5,8 +5,15 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import Avatar from '@mui/material/Avatar';
+import useAPI from './useAPI';
 
 const Chatbot = () => {
+
+  const user = useAPI();
+
+  console.log(user)
+
   const [expanded, setExpanded] = useState(false);
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
@@ -14,9 +21,9 @@ const Chatbot = () => {
   return (
     <div>
        <div className='chat-box'>
-      <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+      <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')} sx={{width: '310px'}} style={{backgroundColor: 'rgb(48,92,212)', color: 'white', borderTopLeftRadius: '10px', borderTopRightRadius: '10px'}}>
         <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
+          expandIcon={<ExpandMoreIcon style={{ color: 'white'}}/>}
           aria-controls="panel1bh-content"
           id="panel1bh-header"
         >
@@ -24,12 +31,12 @@ const Chatbot = () => {
           <Typography sx={{ width: '33%', flexShrink: 0 }}>
             Chat
           </Typography>
-          <Typography sx={{ color: 'text.secondary' }}>I am an accordion</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
-            Aliquam eget maximus est, id dignissim quam.
+        </AccordionSummary >
+        <AccordionDetails style={{backgroundColor: 'white', color: 'black'}}>
+        <Typography sx={{ color: 'text.secondary' }}>
+           {user && user.users.map((u) => <Typography sx={{ color: 'text.secondary' }} style={{p: '10px'}}> {u.name }</Typography>
+           
+           )}
           </Typography>
         </AccordionDetails>
         </Accordion>
